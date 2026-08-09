46cbe16392b9bda0f623e91b26dd3f2a5e5b1dce
CedricDeVon <vaughncedriclipardoaraneta@gmail.com>
2026-08-09 22:19:58 +0800

    deployment attempt of 'v2.0.2'

--- SECURITY.md
+++ SECURITY.md
@@ -4,5 +4,5 @@
 
 | Version | Supported |
 | ------- | ------------------ |
-| = v2.0.1 | :white_check_mark: | 
-| < v2.0.1 | :x: | 
+| = v2.0.2 | :white_check_mark: | 
+| < v2.0.2 | :x: | 
--- qlogicae_logis/v2/project/configuration/about.py
+++ qlogicae_logis/v2/project/configuration/about.py
@@ -8,7 +8,7 @@ DATA: dict[str, dict[str, Any]]= {
     },
     "project-version": {
         "name": "Project Version",
-        "value": "v2.0.1",
+        "value": "v2.0.2",
         "is-tabular": True,
     },
     "project-description": {
